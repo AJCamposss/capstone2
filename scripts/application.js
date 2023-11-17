e9a8d4daf4db98ba184d256b80263a7b9cdc5a43
AJCamposss <adrian.campos869@gmail.com>
2023-11-16 21:01:08 -0700

    Added script to search national parks page

--- scripts/application.js
+++ scripts/application.js
@@ -1,20 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     displayLocations(locationsArray)
 })
-  
 
 function displayLocations (locationsArray) {
-    if('content' in document.createElement('form')) {
-        const locationsContainer = document.querySelector('#content')
-
-        locationsContainer.innerHTML = "";
-
-        locationsContainer.forEach (location => {
-            addLocation (location, locationsContainer)
-        })
-
-    }
-
+    const locationsContainer = document.querySelector()
 }
+  
 
 
