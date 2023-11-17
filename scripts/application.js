1fe7424b9416c5f36102af0bda94b928f243f697
AJCamposss <adrian.campos869@gmail.com>
2023-11-16 20:32:38 -0700

    Made changes to html search national parks and still going through JS

--- scripts/application.js
+++ scripts/application.js
@@ -1,4 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-    displayLocations(locations)
+    displayLocations(locationsArray)
 })
+  
+
+function displayLocations (locationsArray) {
+    if('content' in document.createElement('form')) {
+        const locationsContainer = document.querySelector('#content')
+
+        locationsContainer.innerHTML = "";
+
+        locationsContainer.forEach (location => {
+            addLocation (location, locationsContainer)
+        })
+
+    }
+
+}
+
 
