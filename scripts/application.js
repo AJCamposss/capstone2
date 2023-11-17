9d657f80533ec11443632eb2424d103b67ed1d06
AJCamposss <adrian.campos869@gmail.com>
2023-11-16 21:19:20 -0700

    Stuck on JS

--- scripts/application.js
+++ scripts/application.js
@@ -1,10 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-    displayLocations(locationsArray)
+    
+    displayLocations(locationData);
 })
 
-function displayLocations (locationsArray) {
-    const locationsContainer = document.querySelector()
+function displayLocations (locationData) {
+    const locationsContainer = document.querySelector("#location")
+    locationsContainer.innerHTML = "";
+
 }
-  
+ 
+locationData.forEach(location => {
+    const locationOption = document.createElement("option")
+    locationOption.classList.add("location");
+    locationOption.id = "location-" + location.id;
+    
+    locationsContainer.appendChild(locationOption)
+
+    const locationOptionInfo = document.createElement("Option");
+    locationOption.appendChild(locationOptionInfo)
+
+
+
+})
 
 
