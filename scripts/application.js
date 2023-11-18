9534619ad54d38a658fa15cbaaf01914982a858b
AJCamposss <adrian.campos869@gmail.com>
2023-11-18 15:55:02 -0700

    Made progress to JS, just need to fix it

--- scripts/application.js
+++ scripts/application.js
@@ -1,20 +1,26 @@
-window.onload 
 
-const locations = "locationsArray";
+document.addEventListener("DOMContentLoaded", () => {
+
+    displayLocations(locationsArray);
+})
+
+function displayLocations(locationsArray){
 
 
+locationsArray.forEach(locations => {
+    addLocations(locations);
 
+   });
+
+}
+
+function addLocations(locations) {
     const selectElement = document.getElementById('location');
-
-locations.forEach (function (location) {
     const option = document.createElement('option');
     option.value = location;
     option.text = location;
     selectElement.appendChild(option);
- })
-
-
-
+}
 
 
 
