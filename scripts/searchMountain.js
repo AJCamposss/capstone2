86841e150795ab5162a151c955c0c56924b8e03b
AJCamposss <adrian.campos869@gmail.com>
2023-11-19 19:58:51 -0700

    Added mountain names to dropdown on searh mountain page

--- scripts/searchMountain.js
+++ scripts/searchMountain.js
@@ -2,3 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
     displayMountains(mountainsArray)
 })
 
+function displayMountains(mountainsArray) {
+
+    mountainsArray.forEach(mountain => {
+        addMountain(mountain)
+    })
+}
+
+function addMountain(mountain) {
+    const selectElement = document.getElementById('mountain');
+    const option = document.createElement('option')
+    option.value = mountain;
+    option.text = mountain.name;
+    selectElement.appendChild(option)
+}
