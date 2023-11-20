149b9eb4117af452477902d9567b6861536192c2
AJCamposss <adrian.campos869@gmail.com>
2023-11-19 20:54:07 -0700

    Figured out how to display information on select in JS, close to being down just need to finish making adjustments to my other js files

--- scripts/searchMountain.js
+++ scripts/searchMountain.js
@@ -1,18 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-    displayMountains(mountainsArray)
-})
+    const selectElement = document.getElementById('mountain');
+    displayMountains(mountainsArray);
 
-function displayMountains(mountainsArray) {
+    selectElement.addEventListener('change', function() {
+        const selectedMountainName = this.value; // This gets the mountain name that is selected from dropdown
+
+        // This is to find the selected mountain from our mountainsArray
+        const selectedMountain = mountainsArray.find(mountain => mountain.name === selectedMountainName);
+
+        if (selectedMountain) {
+            displaySelectedMountainInfo(selectedMountain);
+        }
+    });
+});
+
+function displayMountains(mountainsArray) { //this function loops through array and finds 
+    const selectElement = document.getElementById('mountain');
 
     mountainsArray.forEach(mountain => {
-        addMountain(mountain)
-    })
+        const option = document.createElement('option');
+        option.text = mountain.name;
+        selectElement.appendChild(option);
+    });
 }
 
-function addMountain(mountain) {
-    const selectElement = document.getElementById('mountain');
-    const option = document.createElement('option')
-    option.value = mountain;
-    option.text = mountain.name;
-    selectElement.appendChild(option)
+function displaySelectedMountainInfo(selectedMountain) {
+    const mountainInfoElement = document.getElementById('mountainInfo');
+    
+    // Display information about the selected mountain in 'mountainInfo' element
+    mountainInfoElement.querySelector("p").innerText = `Name: ${selectedMountain.name}`
+    
 }
