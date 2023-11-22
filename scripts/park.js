be1b3abde51f888abc00889031416aef012994ea
AJCamposss <adrian.campos869@gmail.com>
2023-11-22 00:54:11 -0700

    Think I finally figured out filtering the search national parks, will go in during our days off to see and change anything

--- scripts/park.js
+++ scripts/park.js
@@ -1,34 +1,87 @@
+document.addEventListener('DOMContentLoaded', () => {
+    stateDropdown();
+    parkTypeDropdown();
+    parkFilter(); 
 
-document.addEventListener("DOMContentLoaded", () => {
-    displayLocations(locationsArray);
-    displayParkTypes(parkTypesArray);
-});
+    let stateSelect = document.querySelector('#state-dropdown');
+    stateSelect.addEventListener('change', parkFilter);
 
+    let parkTypeSelect = document.querySelector('#parkTypeDropdown');
+    parkTypeSelect.addEventListener('change', parkFilter);
+}); 
 
-function displayLocations(locationsArray) {
-    const selectElement = document.getElementById('location');
+function parkTypeDropdown() {
+    const parkTypeDropdown = document.getElementById("parkTypeDropdown");
 
-    locationsArray.forEach(location => {
-        const option = document.createElement('option');
-        option.value = location;
-        option.text = location;
-        selectElement.appendChild(option);
+    if (!parkTypeDropdown) return; 
+
+    parkTypesArray.forEach(type => {
+        const option = new Option(type);
+        parkTypeDropdown.add(option);
+    });
+
+}
+
+function stateDropdown() {
+    const stateDropdown = document.querySelector('#state-dropdown');
+
+    if (!stateDropdown) return; 
+
+    locationsArray.forEach(state => {
+        const option = new Option(state);
+        stateDropdown.add(option);
     });
 }
 
-function displayParkTypes(parkTypesArray) {
-    const selectElement = document.getElementById('parkType');
+function parkFilter() {
+    
+    const stateSelected = document.querySelector('#state-dropdown').value;
+    const parkTypeSelected = document.querySelector('#parkTypeDropdown').value;
+    
+    filteredParks = nationalParksArray;
 
-    parkTypesArray.forEach(parkType => {
-        const option = document.createElement('option');
-        option.text = parkType;
-        selectElement.appendChild(option);
+   if (stateSelected !== 'showAll') {
+    filteredParks = filteredParks.filter(park => park.State === stateSelected);
+   } if (parkTypeSelected !== 'showAll') {
+    filteredParks = filteredParks.filter(park => park.LocationName.includes(parkTypeSelected));
+   } 
+
+   displayParks(filteredParks)
+}
+
+function displayParks(filteredParks) {
+    const parksContain = document.querySelector('#content');
+    parksContain.classList.add("text-center"); 
+
+    parksContain.innerText = ''; 
+
+    filteredParks.forEach(parkFilter => {
+        displayPark(parkFilter, parksContain);
     });
 }
 
-function displaySelectedParkInfo(selectedPark) {
-    const parkInfoElement = document.getElementById('parkInfo');
-    parkInfoElement.querySelector("span").innerText = selectedPark.LocationName
-      
+function displayPark(parkFilter, parksContain) {
+    
+    const parkCard = document.createElement('div'); 
+    parkCard.classList.add('card') 
+    parkCard.id = parkFilter.LocationID; 
+
+    parksContain.appendChild(parkCard); 
+
+    
+    const parkCardBody = document.createElement('div');
+    parkCardBody.classList.add('card-body'); 
+    parkCard.appendChild(parkCardBody); 
+
+    
+    const parkName = document.createElement('h5'); 
+    parkName.classList.add('card-title'); 
+    parkName.innerText = parkFilter.LocationName;
+    parkCardBody.appendChild(parkName); 
+
+    
+    const parkLocation = document.createElement('h6'); 
+    parkLocation.classList.add('card-subtitle'); 
+    parkLocation.innerText = parkFilter.State; 
+    parkCardBody.appendChild(parkLocation);
 }
-  
